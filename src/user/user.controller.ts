11def0a601496c318748bb656ebae72db3b984d8
Lanseria <zhangchao564265135@hotmail.com>
2020-04-04 10:24:17 +0800

    remove log

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, UseGuards, Query } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserDTO } from "./user.dto";
 import { ValidationPipe } from "src/shared/validation.pipe";
+import { User } from "./user.decorator";
+import { AuthGuard } from "src/shared/auth.guard";
 
 @Controller("user")
 export class UserController {
@@ -10,6 +12,12 @@ export class UserController {
   @Get()
   showAllUsers(@Query("current") current: number, @Query("size") size: number) {
     return this.userService.showListByPage(current, size);
+  }
+
+  @Get("mine")
+  @UseGuards(AuthGuard)
+  mine(@User("username") username: string) {
+    return this.userService.read(username);
   }
 
   @Post("login")
